6cbcdf0e7948911c5846331f85fcc6587c3cf292
CamachoBry <bcama007@fiu.edu>
2020-03-11 11:17:23 -0400

    First commit

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_v1_5 as pkc
+from Crypto.Random import new as Random
+from base64 import b64encode, b64decode
+
+app = Flask(__name__)
+
+#Set up the RSA keys
+randon_generator = Random().read
+set_of_keys = RSA.generate(1024, randon_generator)
+
+public_key = set_of_keys.publickey()
+
+# Defining Routes
+@app.route('/')
+def index():
+        return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        message = request.form['message']
+        encrypted = request.form['encrypted']
+        
+        # Validating input
+        if message == '':
+            return render_template('index.html', message='Please enter required fields')
+        elif len(message) > 200 and encrypted == 'true':
+            return render_template('index.html', message='Please insert an string with less than 200 characters')
+        # Encrypting Message        
+        if encrypted == 'true':
+            bit_message = b64encode(message.encode())
+            cyphered_encryption = pkc.new(set_of_keys)
+            encrypted_message = b64encode(cyphered_encryption.encrypt(bit_message)).decode()
+
+            return render_template('index.html' , encrypted=encrypted_message, type='Encrypted')
+        # Decrypting Message
+        else:
+            bit_message = b64decode(message.encode())
+            cyphered_decryption = pkc.new(set_of_keys)
+            encrypted_message = b64decode(cyphered_decryption.decrypt(bit_message,16)).decode()
+            return render_template('index.html' , encrypted=encrypted_message, type='Decrypted')
+
+
+if __name__ == '__main__':
+        
+        app.run()
